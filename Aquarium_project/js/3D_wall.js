c4b9c0a4e8182cbc44c011dd341e7729b76022c0
dogphinx <saydits08@gmail.com>
2021-06-17 23:44:29 +0900

    making_index_stage
    making index wall a little

---
+++ Aquarium_project/js/3D_wall.js
@@ -0,0 +1,33 @@
+(function(){
+    const stageElem = document.querySelector('.stage');
+    const houseElem = document.querySelector('.house');
+    const barElem = document.querySelector('.progress-bar');
+    const mousePos = {
+        x: 0,
+        y: 0
+    };
+    let maxScrollValue;
+
+    function resizeHandler() {
+        maxScrollValue = document.body.offsetHeight - window.innerHeight;
+    }
+
+    window.addEventListener('scroll', function () {
+        const scrollPer = pageYOffset / maxScrollValue;
+        const zMove = scrollPer * 980 - 490;
+        houseElem.style.transform = 'translateZ(' + zMove + 'vw)';
+        barElem.style.width = scrollPer * 100 + '%';
+    });
+
+    window.addEventListener('mousemove', function (e) {
+        mousePos.x = -1 + (e.clientX / window.innerWidth) * 2;
+        mousePos.y = 1 - (e.clientY / window.innerHeight) * 2;
+        + 'deg)';
+        stageElem.style.transform = 'rotateX(' + (mousePos.y * 8) + 'deg) rotateY(' + (mousePos.x * 8) + 'deg)';
+    });
+
+    window.addEventListener('resize', resizeHandler);
+
+    resizeHandler();
+
+})();
